dbc609f3983f8950bc87e0d7cd178d4ca1da214f
nachopaank <ignaciocruzcrespo98@gmail.com>
2025-11-10 13:29:51 +0100

    server

--- server.js
+++ server.js
@@ -1,45 +1,37 @@
-// Run: npm init -y && npm i express socket.io
-
-
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-cors: {
-origin: '*', // lock this down in prod
-}
+const io = new Server(server, { cors: { origin: "*" } });
+
+// Serve the React build folder
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// Catch-all handler to return React's index.html for any other route
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
+// WebRTC signaling
+io.on("connection", (socket) => {
+  console.log("Socket connected:", socket.id);
 
-// Simple room join + relay
-io.on('connection', socket => {
-console.log('Socket connected:', socket.id);
+  socket.on("join", (roomId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("peer-joined", socket.id);
+  });
 
+  socket.on("signal", ({ roomId, data }) => {
+    socket.to(roomId).emit("signal", { from: socket.id, data });
+  });
 
-socket.on('join', roomId => {
-socket.join(roomId);
-const clients = io.sockets.adapter.rooms.get(roomId) || new Set();
-console.log(`Socket ${socket.id} joined ${roomId} (clients=${clients.size})`);
-// inform others
-socket.to(roomId).emit('peer-joined', socket.id);
+  socket.on("disconnect", () => {
+    console.log("Socket disconnected:", socket.id);
+  });
 });
 
-
-socket.on('signal', ({ roomId, data }) => {
-// broadcast the signaling data to others in the room
-socket.to(roomId).emit('signal', { from: socket.id, data });
-});
-
-
-socket.on('disconnect', () => {
-console.log('Socket disconnected:', socket.id);
-});
-});
-
-
-const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => console.log('Signaling server running on', PORT));
+const PORT = process.env.PORT || 10000;
+server.listen(PORT, () => console.log(`Signaling server running on ${PORT}`));
